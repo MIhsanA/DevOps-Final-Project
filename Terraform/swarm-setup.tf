55d262cb54232eeca6f3c223c7afcf5253ec5835
Emiljano <Emiljanokurbiba@gmail.com>
2020-08-05 13:45:03 +0100

    Edited Terraform file.
    The Terraform file now sets up a manager & worker.

--- Terraform/swarm-setup.tf
+++ Terraform/swarm-setup.tf
@@ -15,8 +15,8 @@ resource "azurerm_resource_group" "main" {
 resource "azurerm_virtual_network" "main" {
   name                  = "terravn"
   address_space         = ["10.0.0.0/16"]
-  location              = "uksouth"
-  resource_group_name   = "FinalRG"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
@@ -33,12 +33,24 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
+    subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_network_interface" "worker" {
+  name                = "terraformNIC2"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration2"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "manager" {
   name                  = "swarmmanager"
   location              = "uksouth"
   resource_group_name   = "FinalRG"
@@ -52,13 +64,45 @@ resource "azurerm_virtual_machine" "main" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "managerdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
     computer_name  = "swarmmanagerhost"
+    admin_username = "ekurbiba"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_virtual_machine" "worker" {
+  name                  = "swarmworker"
+  location              = "uksouth"
+  resource_group_name   = "FinalRG"
+  network_interface_ids = ["${azurerm_network_interface.worker.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "workerdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "swarmworker"
     admin_username = "ekurbiba"
     admin_password = "Password1234!"
   }
