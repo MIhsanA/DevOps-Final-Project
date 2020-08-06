6012016be82137e306829c117127622d57fbd9d9
Emiljano <Emiljanokurbiba@gmail.com>
2020-08-06 18:43:06 +0100

    Attempting to fix issue with path

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,9 +1,11 @@
 . ~/.bashrc
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> .bashrc
+source .bashrc
 cd
 sudo apt update -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
-# Simply installing ansible on current user
 pip3 install --user ansible
 cd DevOps-Final-Project
 git pull
