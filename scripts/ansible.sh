8069b8b1322cf699dba33bcc8cb8d1f4d23234d1
Emiljano <Emiljanokurbiba@gmail.com>
2020-08-06 18:33:26 +0100

    updatee ansible.sh

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,4 +1,11 @@
 . ~/.bashrc
 cd
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+# Simply installing ansible on current user
+pip3 install --user ansible
+cd DevOps-Final-Project
+git pull
 cd DevOps-Final-Project/ansible
 pwd
