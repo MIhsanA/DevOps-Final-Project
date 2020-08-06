bf8bc3c5f5cbd31d5c325c259332348d5af706b4
Emiljano <Emiljanokurbiba@gmail.com>
2020-08-06 17:11:21 +0100

    hook update

---
+++ scripts/ansible.sh
@@ -0,0 +1,21 @@
+sudo su - jenkins
+# Making sure ~/.local/bin Exists
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> .bashrc
+source .bashrc
+#installing prerequisites to install ansible
+## python 3 & pip is needed to to install ansible
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+# Simply installing ansible on current user
+pip3 install --user ansible
+# Verifying Ansible is installed will show on logs
+ansible --version
+#pull version
+cd /DevOps-Final-Project
+git pull
+#locate to root
+cd /DevOps-Final-Project/ansible
+#Running inventory & playbooks
+ansible-playbook -i inventory playbook.yaml
