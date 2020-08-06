f8ae9a6f11297ec7146b2cafd3540554bfa5fe2e
Emiljano <Emiljanokurbiba@gmail.com>
2020-08-06 18:00:32 +0100

    update ansible script

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,2 +1,11 @@
-cd
-sudo pwd
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+# Simply installing ansible on current user
+pip3 install --user ansible
+# Verifying Ansible is installed will show on logs
+ansible --version
+cd DevOps-Final-Project
+git pull
+cd DevOps-Final-Project/ansible
+ansible-playbook -i inventory playbook.yaml
