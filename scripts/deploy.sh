7d8636f41a373d8f5873b9ff7ab0677188f32a60
Emiljano <Emiljanokurbiba@gmail.com>
2020-08-07 10:15:57 +0100

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,10 @@ ssh Team2@51.132.240.149 << EOF
 sudo rm -rf DevOps-Practical
 git clone https://github.com/MIhsanA/DevOps-Final-Project -b Development
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 cd
-cd DevOps-Practical
-docker stack deploy --compose-file docker-compose.yaml springappstack
-sudo docker-compose pull
+cd DevOps-Final-Project
 sudo docker volume create springapp
+docker stack deploy --compose-file docker-compose.yaml stackcode
 EOF
 
